d78b9531eabda227a31a3e02ac4d7ff8c0751527
Ilyass Saadi <isaadi@e1r6p3.1337.ma>
2019-12-22 17:14:29 +0100

    Double inclusion protection

--- malloc.h
+++ malloc.h
@@ -6,16 +6,22 @@
 /*   By: isaadi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/19 21:15:29 by isaadi            #+#    #+#             */
-/*   Updated: 2019/12/22 14:59:04 by ael-bahr         ###   ########.fr       */
+/*   Updated: 2019/12/22 17:11:59 by isaadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define malloc xmalloc
-#define free xfree
-#define exit xexit
-#include <stddef.h>
+#ifndef MALLOC_H
+
+# define MALLOC_H
+
+# define malloc xmalloc
+# define free xfree
+# define exit xexit
+# include <stddef.h>
 void	*xmalloc(size_t xbytes);
 void	xfree(void *adr);
 void	leakcheck();
 void	leakcheckfull();
 void	xexit(int x);
+
+#endif
