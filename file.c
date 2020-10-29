649c6b4292edf05bd25759751402bf794f5bbde8
Ilyass Saadi <isaadi@e1r7p6.1337.ma>
2020-10-29 16:12:04 +0100

    Update

--- file.c
+++
@@ -1,6 +0,0 @@
-void func()
-{
-	char *pointer = malloc(1);
-	free(pointer);
-	exit(0);
-}
--- mklib.sh
+++
@@ -1,4 +0,0 @@
-if [ ! $1 ] || [ ! $2 ]
-	echo "Usage: ./compile [1|2|3(MAIN_TYPE)] [LC|LCF] ..."
-	exit;
-fi
